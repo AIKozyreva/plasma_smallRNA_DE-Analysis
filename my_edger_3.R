ecf2c4cba26529f172b6610bc9dab41f196db69e
Anfisa Kozyreva <74992091+AIKozyreva@users.noreply.github.com>
2024-02-21 15:17:49 +0300

    Add files via upload

---
+++ my_edger_3.R
@@ -0,0 +1,30 @@
+#!/usr/bin/env Rscript
+
+library(edgeR)
+
+count_file <- "/mnt/projects/users/aalayeva/smallRNA/smRNA_STAR_RESULT/aligned_deduplicated/featureCounts_results/optional/count_edited_matrix_toGenome.txt"
+count_data <- read.table(count_file, header = TRUE, stringsAsFactors = FALSE, sep = "\t")
+
+gene_id <- count_data$Geneid
+samples_normal <- count_data[, c(7:18)]
+samples_experiment <- count_data[, c(19:30)]
+
+dge_list <- DGEList(counts = cbind(samples_normal, samples_experiment), genes = gene_id)
+
+group <- factor(rep(c("Normal", "Experiment"), each = ncol(samples_normal)))
+
+dge_list <- calcNormFactors(dge_list)
+design_matrix <- model.matrix(~0 + group)
+dge_list <- estimateDisp(dge_list, design_matrix)
+fit <- glmQLFit(dge_list, design_matrix)
+contrast <- makeContrasts(groupExperiment - groupNormal, levels = colnames(design_matrix))
+qlf <- glmQLFTest(fit, contrast = contrast)
+
+results <- topTags(qlf, n = Inf)
+#print(results)
+# Save all results to a file
+write.table(results$table, file = "EDGER_full_results.txt", sep = "\t", quote = FALSE, row.names = FALSE)
+
+# Save summary information about top 25 differentially expressed genes
+top_genes_summary <- results$table[1:25, c("genes", "logFC", "logCPM", "PValue", "FDR")]
+write.table(top_genes_summary, file = "EDGER_topgenes_output.txt", sep = "\t", quote = FALSE, row.names = FALSE)
