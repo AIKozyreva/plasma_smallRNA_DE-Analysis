8846d694b265a4ad7ab703329e75e414a6317ab2
Anfisa Kozyreva <74992091+AIKozyreva@users.noreply.github.com>
2024-02-21 14:59:42 +0300

    DeduplicByFumi+Sorting_for_Genomic&Transcriptomic_files
    DeduplicByFumi+Sorting_for_Genomic&Transcriptomic_files which are in the specific subfolders of working dir.
    
    !Attention. Run this script from your working dir, which include subfolders or change working_dir variable

--- sorting_deduplication.sh
+++ sorting_deduplication.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+#были несколько папок, в них файлы. Из всех, нам нужны только 2 файла - результаты выравнивания на геном и транскриптом. 
+#Оба файла нужно отсортировать по координатам, если он не отсортирован (транскриптомные, геномные сразу отсортированы), 
+#Затем удалить дубликаты (это сделает fumi-tools, опираясь на UMI ридов), 
+#Результаты fumi-tools надо снова отсортировать по координатам (sam sort это делает).
+#Если у вас много папок на вход (образцов), то их тоже лучше задать через цикл или как-то ещё, как я сделала ниже.
 
-# List of directories
-directories=("2scamt_w_UMI_trimmed" "3scamt_w_UMI_trimmed" "4scamt_w_UMI_trimmed" "5scamt_w_UMI_trimmed" "6scamt_w_UMI_trimmed" "10scamt_w_UMI_trimmed" "11scamt_w_UMI_trimmed" "12scamt_w_UMI_trimmed" "13scamt_w_UMI_trimmed" "14scamt_w_UMI_trimmed" "15scamt_w_UMI_trimmed" "16scamt_w_UMI_trimmed" "17scamt_w_UMI_trimmed" "18scamt_w_UMI_trimmed" "19scamt_w_UMI_trimmed" "20scamt_w_UMI_trimmed" "21scamt_w_UMI_trimmed" "23scamt_w_UMI_trimmed" "24scamt_w_UMI_trimmed")
+# Get the current working directory
+working_directory=$(pwd)
 
-# Loop through each directory
-for foldername in "${directories[@]}"
+# Loop through subfolders with the pattern "s_rez"
+for foldername in "${working_directory}"/*s_rez*/
 do
     echo "The folder which is in processing now $foldername"
 
     # Step 2
-    samtools sort -@ 10 -o "${foldername}/${foldername}_Aligned.toTranscriptome.sorted.out.bam" "${foldername}/${foldername}Aligned.toTranscriptome.out.bam"
+    samtools sort -@ 10 -o "${foldername}/${foldername}_Aligned.toTranscriptome.sorted.out.bam" "${foldername}/${foldername}*Aligned.toTranscriptome.out.bam"
 
     # Step 3
     echo "Finishing sorting original transcript file and starting demultiplexing it"
@@ -21,7 +26,7 @@ do
     echo "Finishing demultiplexing transcriptome, starting genome demultiplexing"
 
     # Step 6
-    fumi_tools dedup --threads 10 --memory 10G -i "${foldername}/${foldername}Aligned.sortedByCoord.out.bam" -o "${foldername}/${foldername}_deduplicated_Aligned.toGenome.bam"
+    fumi_tools dedup --threads 10 --memory 10G -i "${foldername}/${foldername}*Aligned.sortedByCoord.out.bam" -o "${foldername}/${foldername}_deduplicated_Aligned.toGenome.bam"
 
     # Step 7
     echo "Start sorting demultiplexed result files"
