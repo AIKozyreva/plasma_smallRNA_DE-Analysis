01f2c5a72d62be7f5ea7fc762d1b241a8f968d44
Anfisa Kozyreva <74992091+AIKozyreva@users.noreply.github.com>
2024-02-21 13:10:59 +0300

    Copying_specific_data_from_several_folders_into_one
    Осторожнее с названиями папок и паттернами в названии целевых файлов

--- copying_result_sorted_dedup_bam.sh
+++ copying_result_sorted_dedup_bam.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#То есть, тут у нас был ряд папочек (2-23)_trimmed, мы в каждую заходим
+#берём файлы, у которых в названии паттерны из строчки 12 и копируем эти файлы в итоговую директорию, которую сами указываем.
 
-# Define the destination directory
-destination_dir="/mnt/projects/users/aalayeva/smallRNA/smRNA_STAR_RESULT/aligned_deduplicated"
+# Define the destination directory, were you want to find your data in future
+destination_dir="/mnt/projects/users/.../aligned_deduplicated"
 
 # Iterate through each directory
-for folder in {2..23}scamt_w_UMI_trimmed; do
+for folder in {2..23}_trimmed; do
     echo "The folder which is in processing now $folder"
 
     # Use find with -exec to copy files
